b217d128a02acf6cf525b995ee405fe2d4a613b6
Ilya Malinowski <malinowski221106@gmail.com>
2024-04-21 12:00:30 +0300

    initial

---
+++ test.py
@@ -0,0 +1,23 @@
+import os
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+
+model = tf.keras.models.load_model('model.keras')
+
+image_number = 0
+while os.path.isfile('digits/digit-' + str(image_number) + '.png'):
+    image_name = 'digits/digit-' + str(image_number) + '.png'
+    try:
+        img = cv2.imread(image_name)[:, :, 0]
+        img = cv2.resize(img, (28, 28))
+        img = np.invert(np.array([img]))
+        prediction = model.predict(img)
+        print(f"This digit {image_name} is probably a {np.argmax(prediction)}")
+        # plt.imshow(img[0], cmap=plt.cm.get_cmap())
+        # plt.show()
+    except:
+        print("Error!")
+    finally:
+        image_number += 1
