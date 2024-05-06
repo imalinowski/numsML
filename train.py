781572f92a1c80ecb445cd8ea14bbce5ab01579f
Ilya Malinowski <malinowski221106@gmail.com>
2024-05-07 02:13:23 +0300

    layers experimants

--- train.py
+++ train.py
@@ -1,8 +1,5 @@
-import os
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
 import tensorflow as tf
+from tensorflow.keras.layers import Flatten, Dense
 
 mnist = tf.keras.datasets.mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
@@ -11,18 +8,21 @@ x_train = tf.keras.utils.normalize(x_train, axis=1)
 x_test = tf.keras.utils.normalize(x_test, axis=1)
 
 model = tf.keras.models.Sequential([])
-model.add(tf.keras.layers.Flatten(input_shape=(28, 28)))
-model.add(tf.keras.layers.Dense(128, activation='relu'))
-model.add(tf.keras.layers.Dense(128, activation='relu'))
-model.add(tf.keras.layers.Dense(10, activation='softmax'))
+model.add(Flatten(input_shape=(28, 28)))
+model.add(Dense(512, activation='relu'))
+model.add(Dense(256, activation='relu'))
+model.add(Dense(128, activation='relu'))
+model.add(Dense(10, activation='softmax'))
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=15)
+history = model.fit(x_train, y_train, epochs=10)
+# model.evaluate(x_test, y_test)
+
+model.save('model.keras')
 
 # todo добавить графики
 # todo сверточная сеть
 # todo добавить валидацию test
-# todo идея ч/б или наоборот
 
-model.save('model.keras')
+# done идея ч/б или наоборот
