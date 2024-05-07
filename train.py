d2ae85e7ea7fa4164eb107b63f95e21851a81d74
Ilya Malinowski <malinowski221106@gmail.com>
2024-05-07 22:42:19 +0300

    convolutional neural network

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Flatten, Dense
+from tensorflow.keras.layers import Flatten, Dense, Conv2D, MaxPooling2D
 
 from ext.ChartsExt import draw_charts
 
@@ -9,8 +9,14 @@ mnist = tf.keras.datasets.mnist
 x_train = tf.keras.utils.normalize(x_train, axis=1)
 x_test = tf.keras.utils.normalize(x_test, axis=1)
 
-model = tf.keras.models.Sequential([])
-model.add(Flatten(input_shape=(28, 28)))
+model = tf.keras.models.Sequential()
+# convolutional
+model.add(Conv2D(28, kernel_size=(3, 3), activation='relu', input_shape=(28, 28, 1)))
+model.add(MaxPooling2D((2, 2)))
+model.add(Conv2D(28, 3, activation='relu'))
+model.add(MaxPooling2D((2, 2)))
+
+model.add(Flatten())
 model.add(Dense(512, activation='relu'))
 model.add(Dense(256, activation='relu'))
 model.add(Dense(128, activation='relu'))
@@ -25,15 +31,16 @@ model.compile(
 history = model.fit(x_train, y_train, epochs=10)
 model.save('model.keras')
 
-
 draw_charts(history)
 print("Evaluate on test data")
 results = model.evaluate(x_test, y_test, batch_size=128)
 print("test loss, test acc:", results)
 
+# todo римский датасет с цифрами
+# todo подстроить тестовые данные
+# todo acc loss автоматизация
 
-# todo сверточная сеть
-
+# done сверточная сеть
 # done добавить валидацию test
 # done добавить графики
 # done идея ч/б или наоборот
